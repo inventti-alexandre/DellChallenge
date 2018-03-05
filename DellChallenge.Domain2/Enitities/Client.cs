1434b10a11cefc4a188ca8ea6c6818b609033dc2
Kaio ramon <kaioramon@gmail.com>
2018-03-05 18:56:30 -0300

    new dell project
    pilot commit

---
+++ DellChallenge.Domain2/Enitities/Client.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DellChallenge.Domain.Enitities
+{
+    [Table("Client")]
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public Genger Gender { get; set; }
+        public Classification Classification { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public DateTime LastPurchase { get; set; }
+        public User Seller { get; set; }
+       
+    }
+}
---
+++ DellChallenge.Web2/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DellChallenge.Domain.Enitities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DellChallenge.Web2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl = null)
+        {
+
+            return View("Index");
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public JsonResult Login([FromBody]User model)
+        {
+            if (ModelState.IsValid)
+            {
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                //var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                //if (result.Succeeded)
+                //{
+                //    _logger.LogInformation("User logged in.");
+                //    return RedirectToLocal(returnUrl);
+                //}
+
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+
+
+            }
+
+            return null;
+        }
+    }
+}
